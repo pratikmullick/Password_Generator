576a5c350492d1de92c5409afc518a21c9ac0e79
Pratik Mullick <pratik.mullick@gmail.com>
2019-08-30 13:35:43 +0530

    Modified core and added country and name list

--- core.py
+++ core.py
@@ -14,7 +14,7 @@ class Generator:
             self.all_chars += {x for x in string.digits}
 
         if p_flag:
-            self.all_chars += {'!','@','#','$','%','^','&','*','_','-','+','=','?','<','>',':'}
+            self.all_chars += {'!','@','#','$','%','^','&','_','-','+','=','<','>',':','?'}
 
     def passgen(self):
         pass_string = ""
@@ -23,3 +23,46 @@ class Generator:
             pass_string += random.choice(self.all_chars)
 
         return pass_string
+
+class Helper:
+
+    def __init__(self, pass_string, filenames):
+        self.pass_string = pass_string
+        self.filelist = filenames
+        self.helper_words = self.wordlist()
+
+    def wordlist(self):
+        h_words = []
+        for filename in self.filelist:
+            with open(filename,"r") as h_file:
+                words = h_file.readlines()
+
+            for word in words:
+                correct = [x for x in word[:-1]]
+                h_words.append(''.join(correct))
+
+        return h_words
+
+    def helper_string(self):
+        delta = []
+        h_string = ""
+
+        for i in self.pass_string:
+            subset = []
+            for word in self.helper_words:
+                if word[0].lower() == i.lower():
+                    subset.append(word)
+            delta.append(subset)
+
+        for subset in delta:
+            h_string += (random.choice(subset) + " ")
+
+        return h_string
+
+
+# Debug
+if __name__ == "__main__":
+    gen = Generator(16,True, False, False).passgen()
+    helper = Helper(gen, ["names.txt","countries.txt"])
+
+    print(helper.helper_string())
