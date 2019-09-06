2bc0f292db315384652acd9a8a6bc5197807e32a
Pratik Mullick <pratik.mullick@gmail.com>
2019-09-06 17:30:02 +0530

    Bug fixes

--- passgen.py
+++ passgen.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 import argparse
+import os
 from core import *
 
 def CommandLine():
 
-    epilog = open("LICENSE",'r').read()
+    cwd = os.getcwd() + "/"
     
     description = "Passgen: A Random Password Generator"
 
-    parser = argparse.ArgumentParser(description=description,epilog=epilog, prog="PassGen")
+    parser = argparse.ArgumentParser(description=description, prog="PassGen")
     parser.add_argument("-s", "--size", type=int, help="Number of characters in generated password")
     parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.50")
     parser.add_argument("-u", "--upper", help="Use uppercase characters", action="store_true", default=True)
@@ -25,15 +26,15 @@ def CommandLine():
         size = int(input("Enter length of password: "))
 
     if args.defaults:
-        gen = Generator(16, True, True, True, True)
+        gen = Generator(32, True, True, True, True)
     else:
-        gen = Generator(size, True, args.uc, args.n, args.sb)
+        gen = Generator(size, True, args.upper, args.numbers, args.symbols)
 
     password = gen.pass_gen()
     print(password)
     
     if args.verbose:
-        memo = Helper(password, ["assets/names.txt","assets/countries.txt"]).mem_string()
+        memo = Helper(password, [cwd + "assets/names.txt",cwd + "assets/countries.txt"]).mem_string()
         print(memo)
 
 if __name__ == "__main__":
